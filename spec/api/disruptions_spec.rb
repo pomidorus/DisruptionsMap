e7ad6aa046498b428842f318d587f8d8c7ad24cc
Andrey Seleznov <andrewaka39@gmail.com>
2016-08-17 18:14:59 +0300

    spec for valid distruptions schema response from api

--- spec/api/disruptions_spec.rb
+++ spec/api/disruptions_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
+require 'support/api_schema_matcher'
 
 describe Disruptions::API, type: :request do
-  context 'GET /api/disruptions' do
-    def dispatch
-      get 'http://localhost:300/api/disruptions'
-    end
-    subject { dispatch }
-    describe 'response' do
+  describe 'GET /api/disruptions' do
+    context 'response status' do
       it 'returns 200 status code' do
-        expect(subject).to eq (200)
+        get 'http://localhost:300/api/disruptions'
+        expect(response.status).to eq (200)
+      end
+    end
+    context 'response schema' do
+      it 'returns the disruptions' do
+        get 'http://localhost:300/api/disruptions'
+        expect(response).to match_response_schema('disruptions')
       end
     end
   end
