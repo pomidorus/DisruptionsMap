c3b79566a933ed48fb828406181bbb90661ad40a
Andrey Seleznov <andrewaka39@gmail.com>
2016-08-17 20:28:51 +0300

    fix response structure for disruption

--- app/models/tims_parser.rb
+++ app/models/tims_parser.rb
@@ -2,6 +2,6 @@ class TimsParser
   DATA_URL = "https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml?app_id=#{ENV['APP_ID']}&app_key=#{ENV['APP_KEY']}"
 
   def self.run
-    [{latlng: [51.503509, 0.001312], popup: 'description 1'}, {latlng: [51.504509, 0.002312], popup: 'description 2'}]
+    [{lat: 51.503509, lng: 0.001312, comment: 'comments'}, {lat: 51.504509, lng: 0.002312, comment: 'comments'}]
   end
 end
