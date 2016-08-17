0aa6f1b68bd6b98c99fecd18d258465340423f20
Andrey Seleznov <andrewaka39@gmail.com>
2016-08-17 16:40:34 +0300

    draw markers on main page using ajax get request to api endpoint

--- app/api/disruptions/api.rb
+++ app/api/disruptions/api.rb
@@ -7,7 +7,10 @@ module Disruptions
     resource :disruptions do
       desc 'Return a list of current disruptions'
       get '/' do
-        {markers: [{latlng: [51.503509, 0.001312], popup: 'Traffic is heavy and slow moving on A12 southbound and A13 westbound towards the southbound tunnel. A12 tailback through Eastway Tunnel to Ruckholt Road (Olympic Park). A13 tailback to Goresbrook Interchange at Dagenham. A102 northbound traffic is queuing from Sun In The Sands Roundabout.'}]}
+        {markers: [
+                    {latlng: [51.503509, 0.001312], popup: 'Traffic is heavy and slow moving on A12 southbound and A13 westbound towards the southbound tunnel. A12 tailback through Eastway Tunnel to Ruckholt Road (Olympic Park). A13 tailback to Goresbrook Interchange at Dagenham. A102 northbound traffic is queuing from Sun In The Sands Roundabout.'},
+                    {latlng: [51.504519, 0.002322], popup: 'Traffic is heavy and slow moving on A12 southbound and A13 westbound towards the southbound tunnel. A12 tailback through Eastway Tunnel to Ruckholt Road (Olympic Park). A13 tailback to Goresbrook Interchange at Dagenham. A102 northbound traffic is queuing from Sun In The Sands Roundabout.'}
+                  ]}
       end
     end
   end
--- spec/api/disruptions_spec.rb
+++ spec/api/disruptions_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 describe Disruptions::API, type: :request do
-
-  describe 'GET /api/disruptions' do
-
+  context 'GET /api/disruptions' do
     def dispatch
       get 'http://localhost:300/api/disruptions'
     end
-
     subject { dispatch }
-
     describe 'response' do
-      it { expect(subject).to eq (200) }
+      it 'returns 200 status code' do
+        expect(subject).to eq (200)
+      end
     end
   end
 end
